627b55c70384817d3b2771076dcc2956f00e19a2
Geri Bylykbashi <gbylykba@uwaterloo.ca>
2023-10-26 22:21:05 -0400

    added geo location and added blue marker with heading

--- charon-frontend/src/components/Map/MapboxMap.js
+++ charon-frontend/src/components/Map/MapboxMap.js
@@ -1,39 +1,48 @@
-import React, { useRef, useEffect, useState } from 'react';
+import React, { useRef, useEffect } from 'react';
 import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
 import './MapboxMap.css';
 import Navbar from '../SideBar/Navbar';
-
 
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_API_KEY;
 
 function MapboxMap() {
     const mapContainer = useRef(null);
     const map = useRef(null);
-    const [lng, setLng] = useState(-70.9);
-    const [lat, setLat] = useState(42.35);
-    const [zoom, setZoom] = useState(9);
+    const geolocateControlRef = useRef(null); // Ref for the GeolocateControl
 
     useEffect(() => {
-        if (map.current) return; // initialize map only once
+        if (map.current) return; // Initialize map only once
+
         map.current = new mapboxgl.Map({
             container: mapContainer.current,
             style: 'mapbox://styles/mapbox/streets-v12',
-            center: [lng, lat],
-            zoom: zoom,
+            center: [ -79.3871, 43.6426 ], // Default center
+            zoom: 13                // Default zoom
         });
 
-        map.current.on('move', () => {
-            setLng(map.current.getCenter().lng.toFixed(4));
-            setLat(map.current.getCenter().lat.toFixed(4));
-            setZoom(map.current.getZoom().toFixed(2));
+        geolocateControlRef.current = new mapboxgl.GeolocateControl({
+            positionOptions: {
+                enableHighAccuracy: true
+            },
+            trackUserLocation: true,
+            showUserHeading: true
         });
-    }, [lng, lat, zoom]);
+
+        map.current.addControl(geolocateControlRef.current);
+
+        // Trigger the GeolocateControl to get the user's location once the map has loaded
+        map.current.on('load', () => {
+            geolocateControlRef.current.trigger();
+        });
+
+    }, []);
 
     return (
         <div>
             <div className="map-container" ref={mapContainer} />
-            {/* Render the Navbar component on top of the map */}
-            <Navbar /> 
+            <Navbar />
         </div>
     );
 }
