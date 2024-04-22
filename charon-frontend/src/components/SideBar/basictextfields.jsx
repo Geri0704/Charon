625700b0976c8a68fe5fdac2a5f7ff9a98e3abd0
Geri Bylykbashi <gbylykba@uwaterloo.ca>
2024-04-22 16:23:07 -0400

    added autofill to the starting and destination. Needs to styled properly still

--- charon-frontend/src/components/SideBar/basictextfields.jsx
+++ charon-frontend/src/components/SideBar/basictextfields.jsx
@@ -1,23 +1,49 @@
-import * as React from 'react';
+// import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
+import { AddressAutofill } from '@mapbox/search-js-react';
+import {useState} from "react";
 
 export default function BasicTextFields() {
+    const [startingPoint, setStartingPoint] = useState('');
+    const [destination, setDestination] = useState('');
+
+    const mapboxAccessToken = process.env.REACT_APP_MAPBOX_API_KEY;
+
     return (
         <Box
             component="form"
             sx={{
-                display: 'flex', // Use flexbox for layout
-                flexDirection: 'column', // Stack children vertically
-                alignItems: 'center', // Center-align items horizontally
-                justifyContent: 'center', // Center-align items vertically
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
                 '& > :not(style)': { m: 1, width: '30ch' },
             }}
             noValidate
             autoComplete="off"
         >
             <TextField id="standard-basic" label="Choose starting point..." variant="standard" />
-            <TextField id="standard-basic" label="Choose destination" variant="standard" />
+            <AddressAutofill accessToken={mapboxAccessToken}>
+                <TextField
+                    id="starting-point"
+                    label="Choose starting point..."
+                    variant="standard"
+                    value={startingPoint}
+                    onChange={(e) => setStartingPoint(e.target.value)}
+                    autoComplete="shipping address-line1"
+                />
+            </AddressAutofill>
+            <AddressAutofill accessToken={mapboxAccessToken}>
+                <TextField
+                    id="destination"
+                    label="Choose destination"
+                    variant="standard"
+                    value={destination}
+                    onChange={(e) => setDestination(e.target.value)}
+                    autoComplete="shipping address-line1"
+                />
+            </AddressAutofill>
         </Box>
     );
 }
